96c141ef0ce0d9827f32d03addf46ba7acf8974f
Ehsan <amid.ehsan@gmail.cm>
2020-01-23 19:25:51 +0800

    initial

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+var btn = document.getElementById("go-button");
+function buttonClicked() {
+
+    console.log("button clicked");
+    btn.removeEventListener("click",buttonClicked);
+    document.getElementById("text").innerHTML = "button clicked"
+}
+
+
+
+btn.addEventListener("click",buttonClicked);
