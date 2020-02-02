8835134621a57718d848adc5c8056dfae229e0e9
Ehsan <amid.ehsan@gmail.cm>
2020-02-02 23:18:03 +0800

    jquery 2/2/2020

--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
 $(document).ready(function(){
 
+  // $('[href="https://google.com"]').on('click', function(event) {
+  //     console.log("linking to google?");
+  //     event.preventDefault();
+  // })
+
+  $(document).on('contextmenu',  function() {
+    return false;
+  })
+
+  $(document).on('mousedown',  function(event) {
+    event.preventDefault();
+    if(event.which == 3) {
+
+        $('.hidden').removeClass('shown');
+
+        if($(event.target).is('img')){
+          $('.saveimg, .newtab').addClass('shown');
+        }
+        else if($(event.target).is('a')){
+          $('.newtab').addClass('shown');
+        }
+
+        console.log(event.pageX,event.pageY);
+
+        $('#context').css({
+          top: event.pageY,
+          left: event.pageX
+        });
+
+        $('#context').fadeIn();
+      return false;
+    }
+    $('#context').fadeOut();
+
+
+  });
 
   $('[data-trigger="dropdown"]').on('mouseenter',function(){
 
@@ -11,7 +47,25 @@ $(document).ready(function(){
           submenu.fadeOut(300);
       })
   });
+  $('#prepend, #append, #replace').on('click',function(e){
+    var el = $(e.currentTarget);
+    var action = el.attr('id');
+    var content = $('.text').val();
 
+    if(action == "prepend") {
+      console.log("Prepending....");
+      $('#main').prepend(content);
+    }
+    else if(action == "append"){
+      console.log("appending....");
+        $('#main').append(content);
+    }
+    else if (action == "replace") {
+      console.log("Replacing....");
+      $('#main').html(content);
+    }
+    $('.text').val('');
+  })
 
 
 
